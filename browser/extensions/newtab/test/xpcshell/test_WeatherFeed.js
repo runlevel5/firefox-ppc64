2b226d363b14fc2cd1aeeaab43c0b8c49a7ef7f6
Maxx Crawford <mcrawford@mozilla.com>
2026-06-18 17:01:57 +0000

    Bug 2044506 - Cancel the Merino session timer in the WeatherFeed Accept-Language xpcshell tests  a=pascalc DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D306854
    
    Differential Revision: https://phabricator.services.mozilla.com/D307600

--- browser/extensions/newtab/test/xpcshell/test_WeatherFeed.js
+++ browser/extensions/newtab/test/xpcshell/test_WeatherFeed.js
@@ -927,6 +927,9 @@ add_task(async function test_shim_fetchWeatherReport_sends_accept_language() {
   MerinoTestUtils.server.reset();
 
   const client = new TemporaryMerinoClientShim("ACCEPT_LANGUAGE_REPORT");
+  // fetchWeatherReport() arms a long-lived Merino session timer. Cancel it so
+  // it doesn't outlive the test and leave a pending timer racing with shutdown.
+  registerCleanupFunction(() => client.resetSession());
   await client.fetchWeatherReport({
     source: "newtab",
     city: "Yokohama",
