1fe0ec5ab47988259b2444ac69156585430ce4e3
Pascal Chevrel <pascal@mozilla.com>
2026-06-18 19:12:56 +0200

    Revert "Bug 2034406 - Preserve vector commands in MozPrintCallback. r=lsalzman" from beta at developer's request a=pascalc
    This reverts commit a49a71cc6e3e96b3e6f50f61f8ba99aeb300873e.

--- layout/generic/nsPageSequenceFrame.cpp
+++ layout/generic/nsPageSequenceFrame.cpp
@@ -13,8 +13,6 @@
 #include "mozilla/ReflowInput.h"
 #include "mozilla/StaticPresData.h"
 #include "mozilla/dom/HTMLCanvasElement.h"
-#include "mozilla/gfx/2D.h"
-#include "mozilla/gfx/DrawEventRecorder.h"
 #include "mozilla/gfx/Point.h"
 #include "mozilla/intl/AppDateTimeFormat.h"
 #include "nsCOMPtr.h"
@@ -551,9 +549,12 @@ static void GetPrintCanvasElementsInFrame(
   }
   for (const auto& childList : aFrame->ChildLists()) {
     for (nsIFrame* child : childList.mList) {
-      // Check if child is a nsHTMLCanvasFrame, and get the canvas element.
-      if (nsHTMLCanvasFrame* canvasFrame = do_QueryFrame(child)) {
-        auto* canvas =
+      // Check if child is a nsHTMLCanvasFrame.
+      nsHTMLCanvasFrame* canvasFrame = do_QueryFrame(child);
+
+      // If there is a canvasFrame, try to get actual canvas element.
+      if (canvasFrame) {
+        HTMLCanvasElement* canvas =
             HTMLCanvasElement::FromNodeOrNull(canvasFrame->GetContent());
         if (canvas && canvas->GetMozPrintCallback()) {
           aArr->AppendElement(canvas);
@@ -562,7 +563,8 @@ static void GetPrintCanvasElementsInFrame(
       }
 
       if (!child->PrincipalChildList().FirstChild()) {
-        if (nsSubDocumentFrame* subdocumentFrame = do_QueryFrame(child)) {
+        nsSubDocumentFrame* subdocumentFrame = do_QueryFrame(child);
+        if (subdocumentFrame) {
           // Descend into the subdocument
           nsIFrame* root = subdocumentFrame->GetSubdocumentRootFrame();
           child = root;
@@ -649,18 +651,16 @@ nsresult nsPageSequenceFrame::PrePrintNextSheet(nsITimerCallback* aCallback,
       UniquePtr<gfxContext> renderingContext = dc->CreateRenderingContext();
       NS_ENSURE_TRUE(renderingContext, NS_ERROR_OUT_OF_MEMORY);
 
-      DrawTarget* referenceDt = renderingContext->GetDrawTarget();
-      if (NS_WARN_IF(!referenceDt)) {
+      DrawTarget* drawTarget = renderingContext->GetDrawTarget();
+      if (NS_WARN_IF(!drawTarget)) {
         return NS_ERROR_FAILURE;
       }
 
       for (HTMLCanvasElement* canvas : Reversed(mCurrentCanvasList)) {
         CSSIntSize size = canvas->GetSize();
-        RefPtr recorder = MakeAndAddRef<gfx::DrawEventRecorderMemory>(nullptr);
-        RefPtr<DrawTarget> canvasTarget =
-            gfx::Factory::CreateRecordingDrawTarget(
-                recorder, referenceDt,
-                gfx::IntRect(gfx::IntPoint(), size.ToUnknownSize()));
+
+        RefPtr<DrawTarget> canvasTarget = drawTarget->CreateSimilarDrawTarget(
+            size.ToUnknownSize(), drawTarget->GetFormat());
         if (!canvasTarget) {
           continue;
         }
