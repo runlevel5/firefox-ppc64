22ebf9dd2c32a647d06f1476002d6fe19ca13045
Nicholas Rishel <nrishel@mozilla.com>
2026-07-01 16:22:38 +0000

    Bug 1977202 - Pre 2: Add async wrapper for nsITimer. r=saschanaz
    Differential Revision: https://phabricator.services.mozilla.com/D302651

--- browser/components/shell/rust/shell_windows/src/util/mod.rs
+++ browser/components/shell/rust/shell_windows/src/util/mod.rs
@@ -2,11 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-//! # Windows Shell
-//!
-//! This crate provides implementations of features necessary for interacting
-//! with the Windows shell.
+//! Utilities for shell_windows.
 
-mod limited_access_features;
-mod secondary_tiles;
-mod util;
+pub(crate) mod async_timer;
