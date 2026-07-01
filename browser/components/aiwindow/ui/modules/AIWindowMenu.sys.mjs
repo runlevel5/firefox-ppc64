be640a57e2e8376413c63eba51a824ed0003c2ec
Atila Butkovits <abutkovits@mozilla.com>
2026-07-01 22:53:12 +0300

    Revert "Bug 2039351 - Fix reopened tab not restoring orevious session conversation r=omarg" for causing failures at browser_aiwindow_memories.js.
    This reverts commit a39bbe221b3d3e27c7c079564908dbb3db73ddde.

--- browser/components/aiwindow/ui/modules/AIWindowMenu.sys.mjs
+++ browser/components/aiwindow/ui/modules/AIWindowMenu.sys.mjs
@@ -130,7 +130,6 @@ export class AIWindowMenu {
         if (url === win.BROWSER_NEW_TAB_URL) {
           AIWindowUI.openInFullWindow(targetBrowser, conversation);
         } else {
-          AIWindow.restoreTabConversation(targetBrowser, conversation);
           AIWindowUI.openSidebar(targetBrowser.documentGlobal, conversation);
         }
       },
