557fd27480f987b7cef12259424c7c9b9d13a63f
Gabriel Luong <gabriel.luong@gmail.com>
2026-07-02 06:15:43 +0000

    Bug 2051492 - Part 2: Display popular sites using a LazyVerticalGrid r=android-reviewers,devota
    Differential Revision: https://phabricator.services.mozilla.com/D309614

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/topsites/ui/AddShortcutBottomSheet.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/topsites/ui/AddShortcutBottomSheet.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
@@ -67,7 +69,11 @@ private fun AddShortcutBottomSheetContent(
     onAddWebsiteClicked: () -> Unit,
     onAddPopularSiteClick: (PopularSite) -> Unit,
 ) {
-    Column(modifier = Modifier.padding(horizontal = FirefoxTheme.layout.space.static200)) {
+    Column(
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .padding(horizontal = FirefoxTheme.layout.space.static200),
+    ) {
         Spacer(modifier = Modifier.height(FirefoxTheme.layout.space.static150))
 
         Text(
